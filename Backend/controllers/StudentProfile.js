de7c39178ff5585b795da2939e79b2932b8574c5
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 01:44:16 +0530

    Update StudentProfile.js

--- Backend/controllers/StudentProfile.js
+++ Backend/controllers/StudentProfile.js
@@ -2,38 +2,29 @@ import bcrypt from "bcrypt";
 import { genToken } from "../config/JWTToken.js";
 import Student from "../models/StudentModel.js";
 
-
 const StudentRiding = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
+    const student = await Student.findOne({ email });
+    if (!student) return res.status(400).json({ message: "Student not Found" });
 
-        const student = await Student.findOne({ email });
-        if (!student) {
-            return res.status(400).json({ message: "Student not Found" });
-        }
+    const isMatch = await bcrypt.compare(password, student.password);
+    if (!isMatch) return res.status(400).json({ message: "Password Incorrect" });
 
-        // compare passwords
-        const isMatch = await bcrypt.compare(password, student.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: "Password Incorrect" });
-        }
+    const token = await genToken(student._id);
 
-        const token = await genToken(student._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+      secure: true, // ⚠️ required for HTTPS
+    });
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            sameSite: "none",
-            secure: true,
-        });
-
-        return res.status(200).json(student);
-
-      
-    } catch (error) {
-        console.log("Student Login error", error);
-        res.status(500).json({ message: "Student Login error" });
-    }
+    return res.status(200).json(student);
+  } catch (error) {
+    console.log("Student Login error", error);
+    res.status(500).json({ message: "Student Login error" });
+  }
 };
 
-export default StudentRiding
+export default StudentRiding;
