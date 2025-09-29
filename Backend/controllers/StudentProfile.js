d9a4d7f9075e7db615a5f7cdcbab16cd49bd69c4
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 01:23:17 +0530

    Update StudentProfile.js

--- Backend/controllers/StudentProfile.js
+++ Backend/controllers/StudentProfile.js
@@ -23,7 +23,7 @@ const StudentRiding = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            sameSite: "None",
+            sameSite: "none",
             secure: true,
         });
 
