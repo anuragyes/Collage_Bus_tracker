231e8640aed6e0b3fff7057dce0e5e233013f496
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 00:52:02 +0530

    Update StudentProfile.js  deepseek ai

--- Backend/controllers/StudentProfile.js
+++ Backend/controllers/StudentProfile.js
@@ -19,6 +19,7 @@ const StudentRiding = async (req, res) => {
 
         const token = await genToken(student._id);
 
+        // Set cookie
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -26,11 +27,46 @@ const StudentRiding = async (req, res) => {
             secure: true,
         });
 
-        return res.status(200).json(student);
+        // Also send token and student data in response
+        return res.status(200).json({
+            ...student._doc,
+            token // Send token in response as backup
+        });
     } catch (error) {
         console.log("Student Login error", error);
         res.status(500).json({ message: "Student Login error" });
     }
 };
 
-export default StudentRiding
+// const StudentRiding = async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+
+//         const student = await Student.findOne({ email });
+//         if (!student) {
+//             return res.status(400).json({ message: "Student not Found" });
+//         }
+
+//         // compare passwords
+//         const isMatch = await bcrypt.compare(password, student.password);
+//         if (!isMatch) {
+//             return res.status(400).json({ message: "Password Incorrect" });
+//         }
+
+//         const token = await genToken(student._id);
+
+//         res.cookie("token", token, {
+//             httpOnly: true,
+//             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+//             sameSite: "None",
+//             secure: true,
+//         });
+
+//         return res.status(200).json(student);
+//     } catch (error) {
+//         console.log("Student Login error", error);
+//         res.status(500).json({ message: "Student Login error" });
+//     }
+// };
+
+// export default StudentRiding
