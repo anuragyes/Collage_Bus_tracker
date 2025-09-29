8b51901c675a5f972a03c463dae98a766c56784b
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 00:59:22 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,8 @@ app.use(
   cors({
     origin: "https://collage-bus-tracker-frontend.onrender.com", // Added multiple origins
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 app.use(express.json());
