082921bb8775449824639e6fd486d1ae052231a7
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 00:18:46 +0530

    Update StudentProfile.js   change

--- Backend/controllers/StudentProfile.js
+++ Backend/controllers/StudentProfile.js
@@ -3,6 +3,7 @@ import { genToken } from "../config/JWTToken.js";
 import Student from "../models/StudentModel.js";
 
 const StudentRiding = async (req, res) => {
+
     try {
         const { email, password } = req.body;
 
@@ -21,9 +22,9 @@ const StudentRiding = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            sameSite: "None",
-            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: false,
         });
 
         return res.status(200).json(student);
@@ -31,6 +32,34 @@ const StudentRiding = async (req, res) => {
         console.log("Student Login error", error);
         res.status(500).json({ message: "Student Login error" });
     }
+    // try {
+    //     const { email, password } = req.body;
+
+    //     const student = await Student.findOne({ email });
+    //     if (!student) {
+    //         return res.status(400).json({ message: "Student not Found" });
+    //     }
+
+    //     // compare passwords
+    //     const isMatch = await bcrypt.compare(password, student.password);
+    //     if (!isMatch) {
+    //         return res.status(400).json({ message: "Password Incorrect" });
+    //     }
+
+    //     const token = await genToken(student._id);
+
+    //     res.cookie("token", token, {
+    //         httpOnly: true,
+    //         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    //         sameSite: "None",
+    //         secure: true,
+    //     });
+
+    //     return res.status(200).json(student);
+    // } catch (error) {
+    //     console.log("Student Login error", error);
+    //     res.status(500).json({ message: "Student Login error" });
+    // }
 };
 
 export default StudentRiding
