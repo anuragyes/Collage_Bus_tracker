55e8d002a1218527adcda91dad233d72a0f21101
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 19:30:59 +0530

    Update StudentRide.js

--- Backend/routes/StudentRide.js
+++ Backend/routes/StudentRide.js
@@ -7,7 +7,7 @@ import StudentRiding from '../controllers/StudentProfile.js';
 export const StudentRide = express.Router();
 
 
-StudentRide.post("/studentRide", isAuth,  StudentRiding);
+StudentRide.post("/studentRide",  StudentRiding);
 
 
 
