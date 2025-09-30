7e8ef4ce988841c9b648326d243100446aedb922
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2025-09-30 11:38:59 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './Context/authContext.js'
-//  import { Toaster } from "react-hot-toast";
+  import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
       <App />
       {/* Toaster should be mounted once in your app */}
-      {/* <Toaster position="top-right" reverseOrder={false} /> */}
+   <Toaster position="top-center"  />
     </AuthProvider>
 )
